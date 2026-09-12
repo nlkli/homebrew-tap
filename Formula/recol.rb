74c3928f6e21a3bb1f9889278685817bb1d154e6
nlkli <nlkli.zvz@gmail.com>
2026-09-12 13:09:48 +0300

    update recol formula; rm script

--- Formula/recol.rb
+++ Formula/recol.rb
@@ -6,7 +6,7 @@ class Recol < Formula
   depends_on arch: :arm64
 
   url "https://github.com/nlkli/recol/releases/download/v0.2.4/recol-macos-arm64.zip"
-  sha256 "b2460288780cd0fda77fc1a4a6b3b358a1bf2ba95d8968df747e82a1912b1d8c"
+  sha256 "bf323feeb19140f8e4eed7079394f75a030954c059619a9e10cf0a19fb037490"
 
   def install
     bin.install "recol"
